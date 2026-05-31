9b018aca9c979cae57f480c3a0450f80839ab1f4
Bart Hoekstra <barthoekstra@gmail.com>
2026-05-31 18:16:26 +0200

    Fix building warnings/errors

--- R/utils_screen.R
+++ R/utils_screen.R
@@ -22,16 +22,16 @@ normalise_text_for_match <- function(s) {
   if (is.null(s) || is.na(s) || !nzchar(s)) {
     return("")
   }
-  # NFD + remove combining marks
+  # NFD + remove combining marks (U+0300-U+036F)
   s <- gsub(
-    "[̀-ͯ]",
+    "[\u0300-\u036f]",
     "",
     iconv(s, from = "UTF-8", to = "UTF-8")
   )
   # Manual umlaut + sharp-s expansion *before* casefold so we don't lose
   # information that iconv-stripping would have folded away.
   s <- chartr(
-    "ÄÖÜäöüß",
+    "\u00c4\u00d6\u00dc\u00e4\u00f6\u00fc\u00df",
     "AOUaous",
     s
   )
